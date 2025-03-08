fab05e7225e10020689cfdeff89224c2de4deb02
Firstclusive-Branding <firstclusiveoffice@gmail.com>
2025-03-08 12:56:05 +0530

    update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,43 +7,74 @@ import { MdOutlineMailOutline } from "react-icons/md";
 
 const ContactUs = () => {
   return (
-    <div className="contact-container">
-      <div className="contact-header">
-        <img src={contactUsImage} alt="Contact Us" className="contact-image" />
-        <h2>Contact Us</h2>
+    <div className="contact-us-container">
+      <div className="contact-us-header">
+        <img
+          src={contactUsImage}
+          alt="Contact Us"
+          className="contact-us-image"
+        />
+        <h2 className="contact-us-title">Contact Us</h2>
       </div>
 
-      <div className="contact-content">
-        <div className="contact-info">
-          <h3>Let's talk with us</h3>
-          <p>
+      <div className="contact-us-content">
+        <div className="contact-us-info">
+          <h3 className="contact-us-subtitle">Let's talk with us</h3>
+          <p className="contact-us-description">
             Questions, comments, or suggestions? Simply fill in the form and
             we’ll be in touch shortly.
           </p>
-          <div className="contact-details">
-            <p>
-              <IoLocationOutline /> <strong>Houston, Texas - 77469</strong>
-            </p>
-            <p>
-              <BiPhoneCall />
-              +1 (832) 757-9277
-            </p>
-            <p>
-              <MdOutlineMailOutline /> info@cadilaglobal.com
-            </p>
+          <div className="contact-us-details">
+            <a href="#" className="contact-us-location">
+              <IoLocationOutline className="contact-us-icons" />{" "}
+              <strong>Houston, Texas - 77469</strong>
+            </a>
+            <a href="tel:+18327579277" className="contact-us-phone">
+              <BiPhoneCall className="contact-us-icons" /> +1 (832) 757-9277
+            </a>
+            <a href="mailto:info@cadilaglobal.com" className="contact-us-email">
+              <MdOutlineMailOutline className="contact-us-icons" />{" "}
+              info@cadilaglobal.com
+            </a>
           </div>
         </div>
 
-        <div className="contact-form">
-          <form>
-            <div className="input-group">
-              <input type="text" placeholder="First Name*" required />
-              <input type="text" placeholder="Last Name*" required />
+        <div className="contact-us-form">
+          <form className="contact-form">
+            <div className="contact-form-name-group">
+              <input
+                type="text"
+                className="contact-form-input"
+                placeholder="First Name*"
+                required
+              />
+              <input
+                type="text"
+                className="contact-form-input"
+                placeholder="Last Name*"
+                required
+              />
             </div>
-            <input type="email" placeholder="Email*" required />
-            <input type="tel" placeholder="Phone Number*" required />
-            <textarea placeholder="Your message..." required></textarea>
-            <button type="submit">Send Message</button>
+            <input
+              type="email"
+              className="contact-form-input"
+              placeholder="Email*"
+              required
+            />
+            <input
+              type="tel"
+              className="contact-form-input"
+              placeholder="Phone Number*"
+              required
+            />
+            <textarea
+              className="contact-form-textarea"
+              placeholder="Your message..."
+              required
+            ></textarea>
+            <button type="submit" className="contact-form-button">
+              Send Message
+            </button>
           </form>
         </div>
       </div>
