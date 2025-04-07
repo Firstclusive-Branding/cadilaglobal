54a3ec63b4964dc8071af04ccae0eb766502c590
Firstclusive-Branding <firstclusiveoffice@gmail.com>
2025-04-07 16:26:34 +0530

    final update

--- src/components/Admin components/AdminDashboard.jsx
+++ src/components/Admin components/AdminDashboard.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "../../styles/Admin Styles/AdminDashboard.css";
 
 const AdminDashboard = () => {
@@ -9,18 +10,56 @@ const AdminDashboard = () => {
   const role = capitalize(storedUser?.role || "User");
   const firstName = capitalize(storedUser?.firstname || "Guest");
 
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setDateTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (date, timeZone) =>
+    date.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+      timeZone,
+    });
+
   return (
     <div className="admin-dashboard">
-      <h1>Hello, {firstName}</h1>
-      <div className="admin-card">
-        <h2>{role} Access</h2>
-        <p>
-          You are logged in as <strong>{role}</strong>
-        </p>
-        <p>
-          This dashboard is your control center. Use the sidebar to manage
-          content, users, or settings.
-        </p>
+      <div className="datetime-group">
+        <div className="datetime">
+          <strong>System time:</strong> {formatTime(dateTime)}
+        </div>
+        <div className="datetime">
+          <strong>CST:</strong> {formatTime(dateTime, "America/Chicago")}
+        </div>
+        <div className="datetime">
+          <strong>EST:</strong> {formatTime(dateTime, "America/New_York")}
+        </div>
+        <div className="datetime">
+          <strong>MST:</strong> {formatTime(dateTime, "America/Denver")}
+        </div>
+        <div className="datetime">
+          <strong>PST:</strong> {formatTime(dateTime, "America/Los_Angeles")}
+        </div>
+      </div>
+      <div className="dashboard-header">
+        <div className="greeting">
+          <h1>Hello, {firstName}</h1>
+        </div>
+
+        <div className="admin-card">
+          <h2>{role} Access</h2>
+          <p>
+            You are logged in as <strong>{role}</strong>
+          </p>
+          <p>
+            This dashboard is your control center. Use the sidebar to manage
+            content, users, or settings.
+          </p>
+        </div>
       </div>
     </div>
   );
